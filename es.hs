f3a30fed0a1726d1d483a8abf575d6718ca19faa
Ting-Yen LAI <banacorn@gmail.com>
2013-04-30 01:23:08 +0800

    1/5 rule

--- es.hs
+++ es.hs
@@ -16,7 +16,7 @@ type Total = Int
 type Success = Int
 data Genotype   = Fixed [Variable] Variance
                 | Uncorrelated [Variable] [Variance]
-                | EinFuenftel [Variable] Variance Success Total deriving (Show, Eq)
+                | EinFuenftel [Variable] Variance (Success, Total) deriving (Show, Eq)
 data Pheonotype = Sphere [Double] deriving (Show, Eq)
 
 instance Ord Pheonotype where
@@ -30,12 +30,12 @@ type Gen = MWC.Gen (Prim.PrimState IO)
 
 tau = 0.7071
 boundary = 0.005
-
+a = 0.9
 
 decode :: Genotype -> Pheonotype
 decode (Fixed variables _) = Sphere variables
 decode (Uncorrelated variables _) = Sphere variables
-decode (EinFuenftel variables _ _ _) = Sphere variables
+decode (EinFuenftel variables _ _) = Sphere variables
 
 evaluate :: Pheonotype -> Double
 evaluate (Sphere sphere) = sum . map square $ sphere
@@ -54,6 +54,22 @@ mutate (Fixed variables sigma, gen) = do
     let variables' = zipWith (\x v -> x + sigma * v) variables variances
 
     return (Fixed variables' sigma, gen)
+
+
+mutate (EinFuenftel variables sigma (success, total), gen) = do
+
+    let successRate = fromIntegral success / fromIntegral total
+    let sigma' = changeSigma sigma successRate
+
+    variances <- replicateM (length variables) (normal 0 1 gen)
+    let variables' = zipWith (\x v -> x + sigma' * v) variables variances
+
+    return (Fixed variables' sigma', gen)
+    where   changeSigma sigma successRate
+                | successRate > 0.2 = sigma / a
+                | successRate < 0.2 = sigma * a
+                | otherwise = sigma
+
 
 mutate (Uncorrelated variables sigmas, gen) = do
 
@@ -76,6 +92,25 @@ generate (father, gen) = do
 -- (1, 1)-ES
 generate' = mutate 
 
+-- (1 + 1)-1/5-ES
+generateEinFuenftel (father, gen) = do
+    (child, gen') <- mutate (father, gen)
+    let result = case child < father of
+            True -> tagSuccess child
+            False -> tagFailure father
+    return (result, gen')
+-- (1, 1)-1/5-ES
+generateEinFuenftel' (father, gen) = do
+    (child, gen') <- mutate (father, gen)
+    let result = case child < father of
+            True -> tagSuccess child
+            False -> tagFailure child
+    return (result, gen')
+
+tagSuccess (EinFuenftel v s (success, total)) = EinFuenftel v s (succ success, succ total)
+tagFailure (EinFuenftel v s (success, total)) = EinFuenftel v s (     success, succ total)
+
+
 run n (x, gen) threshold times elitism
     | times <= n = print n
     | otherwise = do
@@ -86,7 +121,14 @@ run n (x, gen) threshold times elitism
             --print $ "generation " ++ show n ++ "  - " ++ (show $ evaluateG x)
             run (succ n) (x', gen') threshold times elitism
 
-
+runEinFuenftel n (x, gen) threshold times elitism
+    | times <= n = print n
+    | otherwise = do 
+        if evaluateG x <= threshold then do
+            print $ "********** " ++ show n ++ "  - " ++ (show $ evaluateG x)
+        else do
+            (x', gen') <- if elitism then generateEinFuenftel (x, gen) else generateEinFuenftel' (x, gen)
+            runEinFuenftel (succ n) (x', gen') threshold times elitism
 
 get (Fixed v _) = v
 
@@ -101,6 +143,12 @@ go' sigma threshold times elitism = do
     gen <- MWC.initialize (singleton a)
     run 0 (uncorrelated, gen) threshold times elitism
     where uncorrelated = Uncorrelated (replicate 10 1) (replicate 10 sigma)
+
+go'' sigma threshold times elitism = do
+    a <- (MWC.withSystemRandom . MWC.asGenIO $ \gen -> MWC.uniform gen) :: IO Word32
+    gen <- MWC.initialize (singleton a)
+    runEinFuenftel 0 (uncorrelated, gen) threshold times elitism
+    where uncorrelated = EinFuenftel (replicate 10 1) sigma (0, 0)
 
 runF s True = do
     print $ "(1+1)-ES fixed  " ++ show s
@@ -132,4 +180,20 @@ allU = do
     runU 0.1 False
     runU 0.01 False
 
-main = allU
+runE s True = do
+    print $ "(1+1)-ES with 1/5 rule  " ++ show s
+    replicateM_ 10 (go'' s 0.005 10000000 True)
+runE s False = do
+    print $ "(1,1)-ES with 1/5 rule  " ++ show s
+    replicateM_ 10 (go'' s 0.005 10000000 False)
+
+allE = do
+    runE 1 True
+    runE 0.1 True
+    runE 0.01 True
+    runE 1 False
+    runE 0.1 False
+    runE 0.01 False
+
+
+main = allE
