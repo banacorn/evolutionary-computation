140308abbbc0f6559612439fb6451e190cafefa5
Ting-Yen Lai <banacorn@gmail.com>
2013-08-22 03:23:27 +0800

    cabal-dev ghci

--- EC.hs
+++ EC.hs
@@ -1,1 +1,3 @@
 module EC where
+
+import EC.ES
--- EC/es.hs
+++ EC/es.hs
@@ -1,7 +1,7 @@
 
-module ES where
+module EC.ES where
 
---import qualified Control.Monad.Primitive as Prim
---import qualified System.Random.MWC as MWC
---import System.Random.MWC.Distributions (normal)
+import qualified Control.Monad.Primitive as Prim
+import qualified System.Random.MWC as MWC
+import Control.Monad.ST
 
